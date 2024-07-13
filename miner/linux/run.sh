50a473c8e6816958658da7f90732dfc5ab3ae6df
hyh <2667004988@qq.com>
2024-07-13 21:37:16 +0800

    update

--- miner/linux/run.sh
+++
@@ -1,4 +0,0 @@
-chmod +x tnn-miner
-
-./tnn-miner  --spectre --daemon-address 网桥ip  --port 5555 --wallet 钱包地址 --threads 线程数量（跟核心数量一样） --worker-name 矿工名称
-
--- miner/windows/run.cmd
+++
@@ -1,2 +0,0 @@
-Tnn-miner.exe --spectre --daemon-address 网桥ip --port 5555 --wallet 钱包地址 --threads 线程数量（根据自己核心数量）
-
